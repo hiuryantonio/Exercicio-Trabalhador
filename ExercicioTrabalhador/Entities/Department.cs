e8e1043f368ab1924a941e3d79464ab9e89fa0fb
Hiury Gomes <hiury_jp@hotmail.com>
2021-10-01 16:39:50 -0300

    Exercicio trabalhador

---
+++ ExercicioTrabalhador/Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace ExercicioTrabalhador.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
